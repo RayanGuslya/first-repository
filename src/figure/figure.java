9fc5473e56f5b841f2216a5bd05b94ea1ea81632
RayanGuslya <sesmeneev@mail.ru>
2023-05-28 18:47:30 +0300

    add update file

--- src/figure/figure.java
+++ src/figure/figure.java
@@ -2,7 +2,9 @@ package figure;
 //фигура
 import figure.triangle.EquilateralTriangle;
 public class figure {
+    protected String java = "Java";
     public static void main(String[] args){
-
+        EquilateralTriangle equilateralTriangle = new EquilateralTriangle();
+        equilateralTriangle.toString();
     }
 }
--- src/figure/triangle/EquilateralTriangle.java
+++ src/figure/triangle/EquilateralTriangle.java
@@ -2,5 +2,7 @@ package figure.triangle;
 //Равносторонний треугольник
 import figure.figure;
 public class EquilateralTriangle extends figure {
-
+    public String toString(){
+        return java;
+    }
 }
